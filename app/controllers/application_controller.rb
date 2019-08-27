537dc582d14cf90a527efbeb337ab4ab80a86994
shadowfox3735 <normanpan180@gmail.com>
2019-08-27 20:22:14 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < Sinatra::Base
     @user = User.find_by(username: params[:username])
     session[:user_id] = @user.id
     if @user
-      redirect_to erb :
+      redirect_to '/user/home'
   end
 
   get '/account' do
