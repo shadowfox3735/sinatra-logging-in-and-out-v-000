c800912c252143ccd12bfcc97fb5f462210512dc
shadowfox3735 <normanpan180@gmail.com>
2019-08-27 20:42:50 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,8 +1,7 @@
 class Helpers
-  def current_user(session)
+  def self.current_user(session)
     @user = User.find_by_id(session[:user_id])
-    @user
   end
-  def is_logged_in?
+  def self.is_logged_in?
   end
 end
