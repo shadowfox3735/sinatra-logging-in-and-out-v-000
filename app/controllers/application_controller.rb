7598c74da0d710bcd11ab9a975feef54d0b936a6
shadowfox3735 <normanpan180@gmail.com>
2019-08-27 21:20:30 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,8 +11,8 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    @user = User.find_by(:username => params[:username])
-    if @user != nil && @user.password == params[:password]
+    @user = User.find_by(:username => params[:username], :password => params[:password])
+    if @user
       session[:user_id] = @user.id
       redirect to '/account'
     end
